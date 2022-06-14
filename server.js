698eed21f2d869c7dc3108cca51f82e18dc55d89
Nikola Kelava <nikola.kelava94@gmail.com>
2022-06-14 13:52:35 +0200

    Add and implement env variables

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv  = require("dotenv");
 
-
+dotenv.config({ path: "./.env" });
 const app = express();
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("App listening on port 3000!");
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${port}...`);
 });
 
