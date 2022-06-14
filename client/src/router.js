73bb0a42a32ff8589a7d4dd4ac6f65d62c657e32
Nikola Kelava <nikola.kelava94@gmail.com>
2022-06-14 22:19:24 +0200

    Add and implement vue router, change favicon and title

---
+++ client/src/router.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Search from "./components/Search";
+
+
+const routes = [
+    {
+        path: "/",
+        name: "search",
+        component: Search
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+
+export default router;
+
